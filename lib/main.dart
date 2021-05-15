36df5e72a930e2f269d3123d0a83f339856551c0
Aabir Sarkar <factbomberz.1@gmail.com>
2021-05-15 17:26:45 +0530

    hello

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:catalog/pages/home_page_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      home: HomePage()
+    );
+  }
+}
